42e23e042dc4529071164b09273b83c47d9affa3
SongWei <leo_songwei@outlook.com>
2018-01-28 17:20:03 +1100

    stb

---
+++ cutils/usestb.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+unsigned char* read_image(const char* filepath)
+{
+	int x, y, n;
+	int desired_channels = 3; // RGB
+	unsigned char* data = stbi_load(filepath, &x, &y, &n, desired_channels);
+
+	unsigned char* resized_data = (unsigned char*)malloc(128 * 128 * 3);
+	stbir_resize_uint8(data, x, y, 0,
+			resized_data, 128, 128, 0,
+			desired_channels);
+
+	stbi_image_free(data);
+
+	return resized_data;
+}
+
+void free_img(void* data)
+{
+	stbi_image_free(data);
+}
+
+void write_img(void* data)
+{
+	stbi_write_png("testoutput.png", 128, 128, 3, data, 0);
+}
+
+/*
+int main()
+{
+	unsigned char* img_ptr = read_image("./Lenna.png");
+	free(img_ptr);
+	return 0;
+}
+*/
--- cutils/utils.h
+++ cutils/utils.h
@@ -7,3 +7,7 @@ void c_draw_line
  int x1, int y1,
  uint32_t rgb,
  void* pixels, int width);
+
+unsigned char* read_image(const char* filepath);
+void free_img(void* data);
+void write_img(void* data);
